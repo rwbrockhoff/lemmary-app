09498f57baade2b469188209b36403970ceb5b23
rwbrockhoff <brockhoffrw@gmail.com>
2026-05-20 09:09:28 -0500

    feat(layout): add placeholder for mobile view
    Related to #20

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,19 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import '@artifact-ui/core/layers.css';
 import { ErrorBoundary } from '@/components/error-boundary';
+import { MobilePlaceholder } from '@/components/mobile-placeholder';
 import { AppProvider } from '@/providers';
 import { AppRouter } from '@/routes';
 
 createRoot(document.getElementById('root')!).render(
-	<ErrorBoundary>
-		<AppProvider>
-			<AppRouter />
-		</AppProvider>
-	</ErrorBoundary>,
+	<>
+		<MobilePlaceholder />
+		<div className="app-content">
+			<ErrorBoundary>
+				<AppProvider>
+					<AppRouter />
+				</AppProvider>
+			</ErrorBoundary>
+		</div>
+	</>,
 );
